d72fe047d4ee700ab05dfc970410f234b41d354b
Nicolas Felix <nicorox247@icloud.com>
2024-11-15 01:17:01 -0500

    Base Test Runs

--- IntelligentAgent.py
+++ IntelligentAgent.py
@@ -1,3 +1,9 @@
 # Inherit getMove() function
 # Execution $python3 GameManager.py
 
+from BaseAI import BaseAI
+
+class IntelligentAgent(BaseAI):
+    def getMove(self, grid):
+        # Your minimax algorithm will be implemented here
+        pass
